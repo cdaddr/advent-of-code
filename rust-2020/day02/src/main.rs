26c3f1da1d75ee0e3c4332f4365cb6388c282769
Brian Carper <brian@carper.ca>
2020-12-02 00:29:31 -0800

    playing with rust stuff (learning!)

--- rust-2020/day02/src/main.rs
+++ rust-2020/day02/src/main.rs
@@ -1,5 +1,8 @@
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
+
 use regex::Regex;
+use anyhow::Result;
 
 fn main() {
     let input = std::fs::read_to_string("input/day02").unwrap();
@@ -10,41 +13,53 @@ fn main() {
     println!("part2: {}", ans)
 }
 
-fn parse(line: &str) -> (String, i64, i64, String) {
+struct Password {
+    min: i64,
+    max: i64,
+    letter: String,
+    text: String,
+}
+
+fn parse(line: &str) -> Result<Password> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"^(\d+)-(\d+) (.): (\w+)$").unwrap();
     }
 
-    let cap = RE.captures(line).unwrap();
+    let cap: regex::Captures = RE.captures(line).unwrap();
 
-    let min = cap.get(1).unwrap().as_str().parse::<i64>().unwrap();
-    let max = cap.get(2).unwrap().as_str().parse::<i64>().unwrap();
-
-    return (String::from(&cap[3]), min, max, String::from(&cap[4]))
+    return Ok(Password {
+        min: cap[1].parse::<i64>()?,
+        max: cap[2].parse::<i64>()?,
+        letter: String::from(&cap[3]),
+        text: String::from(&cap[4]),
+    });
 
 }
 
 fn part1(input: &str) -> usize {
     let mut valid: usize = 0;
     for line in input.lines() {
-        let (letter, min, max, text) = parse(&line.trim());
-        let appearances: i64 = text.matches(&letter).count() as i64;
-        if appearances >= min && appearances <= max {
-            valid += 1;
+        if let Ok(pw) = parse(&line.trim()) {
+            let appearances: i64 = pw.text.matches(&pw.letter).count() as i64;
+            if appearances >= pw.min && appearances <= pw.max {
+                valid += 1;
+            }
         }
     }
     valid
 }
+
 fn part2(input: &str) -> usize {
     let mut valid: usize = 0;
     for line in input.lines() {
-        let (letter, pos1, pos2, text) = parse(&line.trim());
-        let pos1 = pos1 as usize;
-        let pos2 = pos2 as usize;
-        let found1 = &text[pos1-1..pos1] == letter;
-        let found2 = &text[pos2-1..pos2] == letter;
-        if found1 != found2 { // xor~
-            valid += 1;
+        if let Ok(pw) = parse(&line.trim()) {
+            let pos1 = pw.min as usize;
+            let pos2 = pw.max as usize;
+            let found1 = &pw.text[pos1 - 1..pos1] == pw.letter;
+            let found2 = &pw.text[pos2 - 1..pos2] == pw.letter;
+            if found1 != found2 { // xor~
+                valid += 1;
+            }
         }
     }
     valid
